8dbd2d132864f183dd7ff4f581a2d2200f00bd09
yoav <yoav.shalev67@gmail.com>
2020-05-29 08:02:22 +0300

    adding implemnted functions

--- src/myshell.c
+++ src/myshell.c
@@ -1,10 +1,33 @@
 
 #include "../include/myshell.h"
 
+extern char* prg_name;
+
+void print_directory() {
+    char pwd[PATH_MAX];
+    getcwd(pwd, PATH_MAX);
+
+    printf(BOLD_BLUE "%s" RESET "$ ", pwd + 1);
+}
+
+void print_error_msg(char* commend, char* msg) {
+    if(msg == NULL) {
+        fprintf(stderr, "In " BOLD_GREEN "%s" RESET ": %s:" BOLD_RED " %s... fix it right now!!\n" RESET, prg_name, commend, strerror(errno));
+    }
+    else {
+        fprintf(stderr, "In " BOLD_GREEN "%s" RESET ": %s:" BOLD_RED " %s... fix it right now!!\n" RESET, prg_name, commend, msg);
+    }
+}
+
+int is_added_commend(char* cmd) {
+    if(strncmp(cmd, "cd", 2) == 0)
+        return TRUE;
+    return FALSE;
+}
+
 int execute(cmdLine *pCmdLine) {
     int ret; 
     char* commend = (pCmdLine->arguments)[0];
-    fork();
     ret = execvp(commend, pCmdLine->arguments);
 
     if(ret == -1) {
@@ -16,4 +39,15 @@ int execute(cmdLine *pCmdLine) {
 
     return EXIT_SUCCESS;
 
+}
+
+int change_directory(char* path) {
+    int ret; /*for debuging*/
+
+    if(strcmp(path, "~") == 0) {
+        path = getenv("HOME");
+    }
+
+    ret = chdir(path);
+    return ret;
 }
