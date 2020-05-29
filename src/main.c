14f0d2892381927abfc8c18880d5ef8c45bb719f
yoav <yoav.shalev67@gmail.com>
2020-05-29 08:00:40 +0300

    update file

--- src/main.c
+++ src/main.c
@@ -1,24 +1,44 @@
 #include "../include/myshell.h"
 
+char* prg_name;
+
 int main(int argc, char** argv) {
-    int len;
     cmdLine *parsedLine = NULL;
-    char input[100] = "";
+    pid_t child_pid;
+    int status = 0;
+    char input[PATH_MAX] = "";
+
+    prg_name = argv[0] + 2; /*pass over ./ */
+
     while(1) {
 
-        printf(">>> ");
-        fgets(input, 100, stdin);
+        print_directory();
+        fgets(input, PATH_MAX, stdin); /*get the commend from the user*/
 
-        puts(input);
+        if(strcmp(input, "quit\n") == 0) /*end of the infinite loop of the shell if the commend "quit" is enterd */
+            break;
 
-        len = strlen(input);
-        printf("%d\n", len);
-
-        if(strncmp(input, "quit", len) == 0) /*end of the infinite loop of the shell if the commend "quit" is enterd */
-            return 0;
         parsedLine = parseCmdLines(input);
-        execute(parsedLine);
+        if(is_added_commend(input)) {
+            if(change_directory(parsedLine->arguments[1]) == -1)
+                print_error_msg("cd", NULL);
+
+
+            freeCmdLines(parsedLine);
+            continue;
+        }
+
+        child_pid = fork(); /*create a child process*/
+        
+        if(child_pid == 0) { /*the child born successfuly, so start work with him*/
+            execute(parsedLine);
+        }
+        else {
+            waitpid(-1, &status, 0); /*the main process wait's for his child's to end*/
+        }
+
+        freeCmdLines(parsedLine);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
