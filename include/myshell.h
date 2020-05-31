81f031424cc75e36bece5c438f7a64393dc121ca
yoav <yoav.shalev67@gmail.com>
2020-05-31 08:20:10 +0300

    adding declers of history functions

--- include/myshell.h
+++ include/myshell.h
@@ -14,9 +14,12 @@ file: myshell.h
 #include <unistd.h>
 #include <errno.h> /*for display an error msg*/
 #include "../include/LineParser.h"
+#include "../include/linklist.h"
 
 #define TRUE 1
 #define FALSE 0
+#define CAPACITY 20 
+#define MAX_COMMAND_LENGTH 128
 
 #define PATH_MAX 100
 #define BOLD_RED   "\033[1;31m"
@@ -24,16 +27,22 @@ file: myshell.h
 #define BOLD_BLUE  "\033[1;34m"
 #define RESET "\033[0m"
 
+char** history_arr;
+
 void print_directory();
 void print_error_msg(char* commend, char* msg);
 
 /*receives a parsed line and invokes the
  command using the proper system call*/
 int execute(cmdLine *pCmdLine);
-
 int change_directory(char* path);
-
 int is_added_commend(char* cmd);
+
+void add_to_history(char* cmd, int lastcmd);
+list* make_list(char** cmd_arr, int len);
+
+void init_history();
+void free_history();
 
 
 #endif
