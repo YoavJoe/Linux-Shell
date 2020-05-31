5b7cfac14df5e5d0fed189dd89a5332a3357b452
yoav <yoav.shalev67@gmail.com>
2020-05-31 08:24:10 +0300

    adding history commend option

--- src/main.c
+++ src/main.c
@@ -5,28 +5,42 @@ char* prg_name;
 int main(int argc, char** argv) {
     cmdLine *parsedLine = NULL;
     pid_t child_pid;
-    int status = 0;
+    int status = 0, lastcmd = 0;
+    int select_history = FALSE; /*flag for build link list for history*/
     char input[PATH_MAX] = "";
+    list* history = NULL;
+    
 
     prg_name = argv[0] + 2; /*pass over ./ */
 
-    while(1) {
+    init_history();
 
+    while(1) {
         print_directory();
+
         fgets(input, PATH_MAX, stdin); /*get the commend from the user*/
 
         if(strcmp(input, "quit\n") == 0) /*end of the infinite loop of the shell if the commend "quit" is enterd */
             break;
-
+        
         parsedLine = parseCmdLines(input);
         if(is_added_commend(input)) {
-            if(change_directory(parsedLine->arguments[1]) == -1)
-                print_error_msg("cd", NULL);
-
-
+            if(strncmp(input, "cd", 2) == 0) {
+                change_directory(parsedLine->arguments[1]);
+                add_to_history(input, lastcmd);
+                lastcmd++;
+            }
+            else if(strcmp(input, "history\n") == 0) {
+                history = make_list(history_arr, lastcmd);
+                list_print(history);
+                select_history = TRUE;
+            }
             freeCmdLines(parsedLine);
             continue;
         }
+
+        add_to_history(input, lastcmd);
+        lastcmd++;
 
         child_pid = fork(); /*create a child process*/
         
@@ -39,6 +53,12 @@ int main(int argc, char** argv) {
 
         freeCmdLines(parsedLine);
     }
+    /*if there no commend so no need to free 
+    list that haven't been allocated*/
+    if(select_history == TRUE) 
+        free_list(history);
+
+    free_history();
 
     return EXIT_SUCCESS;
 }
