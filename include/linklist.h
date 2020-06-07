36a69729945d627032f220093074259b6b0ef428
yoav <yoav.shalev67@gmail.com>
2020-06-07 08:53:00 +0300

    adding delete declareion

--- include/linklist.h
+++ include/linklist.h
@@ -21,6 +21,9 @@ void add_to_environment(char* name, char* value);
 list* list_append(list* lst, list* data); /*adds the commend to history list*/
 char* get_command(int command_location); /*get the command from history list*/
 list* make_new_link(char* command);
+int is_environment_empty();
+void delete_from_environment(char* name);
+char* find_value(char* name);
 list* link_to_environment(char* name, char* value);
 int is_exists(char* name, char* value);
 void print_history();
