a84cfea3d3a992555d07b719412a6dd42e1f53e9
yoav <yoav.shalev67@gmail.com>
2020-06-25 11:29:09 +0300

    adding pipeline.c file and function's implementation

---
+++ src/pipeline.c
@@ -0,0 +1,66 @@
+/*
+*******************
+file: linklist.c
+*******************
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "../include/LineParser.h"
+#include "../include/pipeline.h"
+#include "../include/utils.h"
+
+int **create_pipes(int n) {
+    int **pipes = (int**)malloc(sizeof(int) * n);
+    int i;
+    int* pipefd;
+    for(i = 0; i < n; i++) {
+        pipefd = (int*)malloc(2 * sizeof(int));
+        if(pipe(pipefd) == -1) {
+            print_error_msg("pipe", NULL);
+            exit(EXIT_FAILURE);
+        }
+        pipes[i] = pipefd;
+    }
+    return pipes;
+}
+
+void release_pipes(int **pipes, int n) {
+    int i;
+    for(i = 0; i < n; i++) {
+        free(pipes[i]);
+    }
+}
+
+int *left_pipe(int **pipes, cmdLine* pCmdLine) {
+    int* pipe = NULL;
+    int idx = pCmdLine->idx;
+    if(idx > 0)
+        pipe = pipes[idx - 1];
+    return pipe;
+}
+
+int *right_pipe(int **pipes, cmdLine *pCmdLine) {
+    int* pipe = NULL;
+    int idx = pCmdLine->idx;
+    if(pCmdLine->next != NULL)
+        pipe = pipes[idx];
+    return pipe;
+}
+
+void handle_pipe() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
