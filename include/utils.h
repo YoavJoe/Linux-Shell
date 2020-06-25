7500af4c0ecdaceacf9218ecb89ffb3d0b451128
yoav <yoav.shalev67@gmail.com>
2020-06-25 12:02:57 +0300

    adding decomantion and is_digit and is_number declaration

--- include/utils.h
+++ include/utils.h
@@ -17,8 +17,21 @@ file: utils.h
 #define BOLD_BLUE  "\033[1;34m"
 #define RESET "\033[0m"
 
+/*This function print the currnet directory path*/
 void print_directory();
+
+/*This function receives the command and the 
+error msg and print it in custom format*/
 void print_error_msg(char* command, char* msg);
+
+/*This function receives a cmd and chack if 
+it's one of the added commands*/
 int is_added_command(char* cmd);
+
+/*This function receives char and chack if it's digit*/
+int is_digit(char c);
+
+/*This function receives a string and chack if it's a number*/
+int is_number(char* num);
 
 #endif
