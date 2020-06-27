5d7a437594f8357e03b0ee3a3e316638b786e188
yoav <yoav.shalev67@gmail.com>
2020-06-27 09:46:19 +0300

    change check_environment name and add function declare

--- include/myshell.h
+++ include/myshell.h
@@ -6,14 +6,9 @@ file: myshell.h
 #ifndef _MYSHELL_H
 #define _MYSHELL_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/types.h> /*for fork*/
-#include <sys/wait.h>
-#include <unistd.h>
 #include "../include/LineParser.h"
 #include "../include/linklist.h"
+#include "../include/pipeline.h"
 #include "../include/utils.h"
 
 char* prg_name = "";
@@ -21,8 +16,9 @@ char* prg_name = "";
 /*receives a parsed line and invokes the
  command using the proper system call*/
 int execute(cmdLine *pCmdLine);
-void check_environment(cmdLine* parsedLine);
+void check_internal_environment(cmdLine* parsedLine);
 int change_directory(char* path);
+void execute_added_command(cmdLine* cmd);
 int main(int argc, char** argv);
 
 #endif
