6b9c70fb645e6f97273f9c08c79609d1bbd7cfbc
yoav <yoav.shalev67@gmail.com>
2020-06-27 09:47:54 +0300

    implement execute_added_command

--- src/myshell.c
+++ src/myshell.c
@@ -3,6 +3,13 @@
 file: myshell.c
 *******************
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h> /*for fork*/
+#include <sys/wait.h>
+#include <unistd.h>
 #include "../include/myshell.h"
 
 int last_command = 0; /*counter the amount of command's in list*/
@@ -34,10 +41,10 @@ int change_directory(char *path) {
     return ret;
 }
 
-void check_environment(cmdLine* parsedLine) {
+void check_internal_environment(cmdLine* parsedLine) {
     char* arg = NULL, *value = NULL;
     int i;
-    for(i = 0; i < parsedLine->argCount; i++) {
+    for(i = 1; i < parsedLine->argCount; i++) {
         if(is_environment_empty())
             return;
         arg = parsedLine->arguments[i];
@@ -51,13 +58,30 @@ void check_environment(cmdLine* parsedLine) {
     }
 }
 
+void execute_added_command(cmdLine* cmd) {
+    char* command = cmd->arguments[0];
+    if(strcmp(command, "cd") == 0)
+        change_directory(cmd->arguments[1]);
+    else if(strcmp(command, "history") == 0)
+        print_history();
+    else if(strcmp(command, "set") == 0) 
+        add_to_environment(cmd->arguments[1], cmd->arguments[2]);
+    else if(strcmp(command, "env") == 0)
+        print_environment();
+    else if(strcmp(command, "delete") == 0)
+        delete_from_environment(cmd->arguments[1]);
+}
+
 int main(int argc, char** argv) {
     cmdLine *parsedLine = NULL;
-    pid_t child_pid;
+    pid_t pid;
     int status = 0;
     int has_history = FALSE; /*flag for build link list for history*/
-    char input[PATH_MAX] = "", *selected_command;
+    char input[PATH_MAX] = "";
+    char* selected_command;
     int location = 0;
+    int pip[2];
+    int reader, writer;
     
     prg_name = argv[0] + 2; /*pass over ./ */
 
@@ -72,8 +96,9 @@ int main(int argc, char** argv) {
         if(strcmp(input, "quit\n") == 0) /*end of the infinite loop of the shell if the command "quit" is enterd */
             break;
 
-        if(strncmp(input, "!", 1) == 0) {
-            location = atoi(memset(input, '0', 1));
+        if(input[0] == '!') { /* Eli: input[0] == '!' */
+            if(is_number(input + 1))
+                location = atoi(input + 1); /* atoi(input+1) if (input+1) is integer;*/
             if(location > last_command) {
                 print_error_msg("!", "non-existing log index is invoked");
                 continue;
@@ -85,35 +110,17 @@ int main(int argc, char** argv) {
         add_to_history(input);
         last_command++;
         parsedLine = parseCmdLines(input);
-        check_environment(parsedLine);
+        check_internal_environment(parsedLine);
 
         if(is_added_command(input)) {
-            if(strncmp(input, "cd", 2) == 0)
-                change_directory(parsedLine->arguments[1]);
-            else if(strcmp(input, "history\n") == 0) {
-                print_history();
-            }
-            else if(strncmp(input, "set", 3) == 0) 
-                add_to_environment(parsedLine->arguments[1], parsedLine->arguments[2]);
-            else if(strcmp(input, "env\n") == 0) {
-                print_environment();
-            }
-            else if(strncmp(input, "delete", 6) == 0)
-                delete_from_environment(input + 7); 
-            
+            execute_added_command(parsedLine);
             freeCmdLines(parsedLine);
             continue;
         }
-
-        child_pid = fork(); /*create a child process*/
+        if(pipe(pip) == -1)
+            print_error_msg("pipe", NULL);
         
-        if(child_pid == 0) { /*the child born successfuly, so start work with him*/
-            execute(parsedLine);
-        }
-        else {
-            waitpid(-1, &status, 0); /*the main process wait's for his child's to end*/
-        }
-
+        
         freeCmdLines(parsedLine);
     }
 
